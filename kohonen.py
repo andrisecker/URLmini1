e1f6009da29bc991267ba797b1106fbbcd920923
András Ecker <ecker.andris@gmail.com>
2016-11-30 11:53:03 +0100

    runs with the miniproject data

--- kohonen.py
+++ kohonen.py
@@ -5,13 +5,15 @@ authors: Sharbatanu Chatterjee, Andras Ecker
 
 import numpy as np
 import matplotlib.pylab as plb
+import matplotlib.pyplot as plt
 from helpers import *
 from figures import *
 
 
-def som_step(centers, data, neighbor, eta, sigma):
+def som_step(size_k, centers, data, neighbor, eta, sigma):
 	"""
 	Performs one step of the sequential learning for a self-organized map (SOM).
+	:param size_k:
     :param centers:  matrix - cluster centres. (center x dimension) (in this case dim=2)
 	:param data: vector - the actually presented datapoint to be presented in this timestep
 	:param neighbor: matrix - the coordinates of the centers in the desired neighborhood.
@@ -19,11 +21,10 @@ def som_step(centers, data, neighbor, eta, sigma):
     :param sigma: the width of the gaussian neighborhood function. (Effectively describing the width of the neighborhood)
 	:return updated centers
     """
-    
-	size_k = int(np.sqrt(len(centers)))
-    
+
 	# find the best matching unit via the minimal distance to the datapoint
 	b = np.argmin(np.sum((centers - np.resize(data, (size_k**2, data.size)))**2, 1))
+
 	# update winner
 	centers[b, :] += eta * (data - centers[b, :])
 
@@ -42,21 +43,23 @@ def som_step(centers, data, neighbor, eta, sigma):
 	return centers
 
 
-def kohonen(size_k, sigma, eta, tmax, show=True, n_clust=1, size_c=100, data_range=10, var_c=4, dim=2):
+def kohonen(size_k, sigma, eta, tmax, data_range=10, dim=2, show_progress=True, n_clust=1, size_c=100, var_c=4):
 	"""
-	Example for using create_data, plot_data and som_step
+	Learning in Kohonen map (using create_data, plot_data and som_step)
 	:param size_k: size of the Kohonen map 
 	:param sigma: width of the neighborhood
 	:param eta: learning rate
-	:param tmax: max iteration
-	:param show: dispaly figure in every iteration (show=False, faster-just saves the final figure)
-	:param n_clust, size_c, data_range, var_c, dim: params of create_data -> see there (helpers.py)
+	:param tmax: max iteration  #TODO make a better convergence crit!
+	:param data_range: 255 for digits (miniproject), see create_data in helpers.py (exc6)
+	:param dim: 28*28 for digits (miniproject), see create_data in helpers.py (exc6)
+	:param show_progress: dispaly figure in every iteration (show_progress=False, faster-just saves the final figure)
+	:param n_clust, size_c, var_c: params of create_data in helpers.py (exc6)
 	"""
 
 	plb.close('all')
-
-	# create data
-	data = create_data(n_clust, data_range, var_c, size_c, dim)
+	
+	#data = create_data(n_clust, data_range, var_c, size_c, dim) # create data (exc6)
+	data = load_data("Andras Ecker")  # load in numbers (miniproject1)
 	dy, dx = data.shape
     
 	# initialise the centers randomly
@@ -68,41 +71,58 @@ def kohonen(size_k, sigma, eta, tmax, show=True, n_clust=1, size_c=100, data_ran
 	# set the random order in which the datapoints should be presented
 	i_random = np.arange(tmax) % dy
 	np.random.shuffle(i_random)
-	
-	plb.ion() # turn "interaction" on in pylab	
+		
 	handles = None # init handles
+	if show_progress: # for fancy iterative plot ! (set show=False to not show it)
+		plb.ion() # turn "interaction" on in pylab	
+	
 
+	# core of the code:
 	for it, i in enumerate(i_random):
-		centers = som_step(centers, data[i,:], neighbor, eta, sigma)  # update centers
-		handles = plot_data(centers, data, neighbor, sigma, eta, it, tmax, handles)  # update plot
-		if show:
+		centers = som_step(size_k, centers, data[i,:], neighbor, eta, sigma)  # update centers
+		#handles = plot_data(centers, data, neighbor, sigma, eta, it, tmax, handles)  # update plot
+		if show_progress:	
 			plb.draw()
 			plb.show()
 			plb.pause(1e-7)
 		
+	
+	if show_progress:
 	# leave the window open at the end of the loop
-	plb.draw()
-	plb.show()
-	plb.waitforbuttonpress(timeout=3)
+		plb.draw()
+		plb.show()
+		plb.waitforbuttonpress(timeout=3)
 
-	# save figures	
-	from datetime import datetime as dt
-	time_ = dt.now().strftime('%Y-%m-%d_%H:%M:%S')
-	figName = 'figures/kohonen_it:%s(%s).jpg'%(tmax, time_)
-	plb.savefig(figName)
+		# save figure
+		from datetime import datetime as dt
+		time_ = dt.now().strftime('%Y-%m-%d_%H:%M:%S')
+		figName = 'figures/kohonen_it:%s(%s).jpg'%(tmax, time_)
+		plb.savefig(figName)
 
 	print("-----terminated; figure saved!-----")
-    
+
+
+	# for visualization, you can use this:
+	fig = plt.figure(figsize=(10,8))
+	for i in range(size_k**2):
+		ax = fig.add_subplot(size_k, size_k, i+1)
+        
+		ax.imshow(np.reshape(centers[i,:], [28, 28]),interpolation='bilinear')
+		#plt.axis('off')
+
+	plt.show()
 
 
 if __name__ == "__main__":
-
-	# set the width of the neighborhood (via the width of the gaussian that describes it)
+	
 	size_k = 6
+	
+	# set the width of the neighborhood (via the width of the gaussian that describes it)
 	sigma_factor = 6
-	sigma = np.sqrt(2.0 * size_k**2) / sigma_factor  # in the "real world" network size and the size of the neighbourhod should be correlated
-	sigma = 1  # hovewer, in the project they ask us to be stupid and play around and hard code sigma (to 1,3,5)
+	#sigma = np.sqrt(2.0 * size_k**2) / sigma_factor  # in the "real world" network size and the size of the neighbourhod should be correlated (exc6)
+	sigma = 3  # hovewer, in the project they ask us to be stupid and play around and hard code sigma (to 1,3,5 - miniproject)
 
-	kohonen(size_k=size_k, sigma=sigma, eta=0.3, tmax=400)
+	#kohonen(size_k=size_k, sigma=sigma, eta=0.3, tmax=400)  # for exc6
+	kohonen(size_k=size_k, sigma=sigma, eta=0.3, tmax=1000, data_range=255, dim=28*28, show_progress=False)
 
 
